ace03c79f043daabe14d1967371d5aa6ab0475af
LinTrieu <36490540+LinTrieu@users.noreply.github.com>
2019-07-01 16:13:05 +0100

    Adds Sort method

--- timing_framework/Program.cs
+++ timing_framework/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace timing_framework
 {
@@ -6,7 +8,34 @@ namespace timing_framework
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("------------------");
+            Console.WriteLine("List of Integers");
+            ListOfIntegers().ForEach(Console.WriteLine);
+            Console.WriteLine("------------------");
+            Console.WriteLine("Sorted List");
+            SortsList().ForEach(Console.WriteLine);
+            Console.WriteLine("------------------");
         }
+
+        public static List<int> ListOfIntegers()
+        {
+            List<int> numbers = new List<int> { 5, 2, 3, 1, 4 };
+            return numbers;
+        }
+
+        public static List<int> SortsList()
+        {
+            List<int> sortedList = ListOfIntegers();
+            sortedList.Sort();
+            return sortedList;
+        }
+
+        //public void WriteToConsole(IEnumerable items)
+        //{
+        //    foreach (object i in items)
+        //    {
+        //        Console.WriteLine(i);
+        //    }
+        //}
     }
 }
